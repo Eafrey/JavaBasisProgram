6b57ca3326e888e1e17fda72afffd0764180e87d
chensen <1019812436@qq.com>
2018-04-18 21:02:48 +0800

    section1 practiceC run succussful

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,21 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> res = new ArrayList<>();
+
+        List<String> col2 = collection2.get("value");
+
+        for(String str : collection1) {
+            if(col2.contains(str)) {
+                res.add(str);
+            }
+        }
+        return res;
     }
 }
