179776459a2f99b27392c41c91834c69aa1462f3
chensen <1019812436@qq.com>
2018-04-18 22:46:22 +0800

    section3 practiceD run successful

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,37 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> map = new HashMap<>();
 
-        return null;
+        for(String str : collectionA) {
+            if(str.contains("-")) {
+                String[] strs = str.split("-");
+                map.put(strs[0], Integer.parseInt(strs[1]));
+            } else {
+                if(map.containsKey(str)) {
+                    int time = map.get(str);
+                    map.put(str, ++time);
+                } else {
+                    map.put(str, 1);
+                }
+            }
+        }
+        Map<String, Integer> res = new HashMap<>();
+
+        List<String> list2 = object.get("value");
+        for(String str : map.keySet()) {
+            if(list2.contains(str)) {
+                res.put(str, map.get(str) - map.get(str)/3);
+            } else {
+                res.put(str, map.get(str));
+            }
+        }
+        return res;
     }
 }
